934807c5f3033ca30edcbfd1cc5141c28853220f
lakshanlach <97869720+lakshanlach@users.noreply.github.com>
2022-04-24 17:47:36 +0530

    Add files via upload

---
+++ main1.js
@@ -0,0 +1,16 @@
+function preload(){
+
+}
+
+function setup(){
+canvas =  createCanvas(700,600);
+canvas.center()
+}
+   
+function draw(){
+
+}
+
+function modal(){
+    window.location=("modal.html")
+}
